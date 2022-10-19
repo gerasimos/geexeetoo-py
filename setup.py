c3837ed4034fc1ad582548906577878fce1d8772
Gerasimos Xydas <gerasimos.xydas@gmail.com>
2022-10-19 12:46:38 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name='geexeetoo',
     version='0.1.0',
     author='Geexee',    
-    py_modules=find_packages(include=['console.*']),
+    py_modules=find_packages(include=['geexeetoo.*']),
 )
